b2a2b82180eacc19cb5f7d17cdce3f3acc1f6f2e
xsean2020 <sean2020@gmail.com>
2020-04-29 13:18:04 +0800

    循环变量作用于闭包引起的值错误

--- router/echo.go
+++ router/echo.go
@@ -18,8 +18,11 @@ func AddRoutersToEcho(e *echo.Echo) {
 		for _, router := range routers {
 			// 参数检查
 			if router.handler.Type.In(1).Implements(ctx) {
+				// copy
+				var handler = router.handler
+				var ctl = controller
 				g.Add(router.method, router.path, func(ctx echo.Context) error {
-					err, _ := router.handler.Func.Call([]reflect.Value{reflect.ValueOf(controller), reflect.ValueOf(ctx)})[0].Interface().(error)
+					err, _ := handler.Func.Call([]reflect.Value{reflect.ValueOf(ctl), reflect.ValueOf(ctx)})[0].Interface().(error)
 					return err
 				})
 				log.WithField("method", router.method).WithField("path", controller.Group()+router.path).WithField("controller", ttype.String()).Info("register router")
